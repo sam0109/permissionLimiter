78e4c688bd1ef6d9716e3c9f68ae5c3a6a72c8da
Sam Sobell <sam.sobell@gmail.com>
2013-05-23 13:40:03 -0700

    logic error

--- PermissionChanger.py
+++ PermissionChanger.py
@@ -4,12 +4,11 @@ import os
 import re
 
 def replace_line(file_name, line_num, text):
-    lines = open(file_name, 'r').readlines()
-    lines[line_num] = text
-    lines.close()
-    out = open(file_name, 'w')
-    out.writelines(lines)
-    out.close()
+	lines = open(file_name, 'r').readlines()
+	lines[line_num] = text
+	out = open(file_name, 'w')
+	out.writelines(lines)
+	out.close()
 
 def readPermissions():
 	permission = {}
@@ -22,14 +21,18 @@ def readPermissions():
 			line = re.sub('" />\n$','',line)
 			permission[j] = line
 		j += 1
+
 	return permission
+print '\npermissions currently in the .apk file in this directory:\n'
 
 permission = readPermissions()
-print '\npermissions currently in the .apk file in this directory:\n'
 for value in permission:
 	print str(value) + str(permission[value])
+
 removePermission = raw_input('permissions to remove (sparated by commas, e.g. "1,5,12"):')
 print ('removing '+str(removePermission))
-removePermission.split(',')
-for permissionToRemove in removePermission:
-	replace_line('./TestApp/AndroidManifest.xml',permissionToRemove,'')
+removePermission = removePermission.split(',')
+
+for lineNumber in removePermission:
+	print(type(lineNumber))
+	replace_line('./TestApp/AndroidManifest.xml',int(lineNumber),'')
